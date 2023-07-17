19d3a7a476087f58c8b0c5330e2f3bb54ea2f217
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-07-17 12:20:06 -0300

    Criar lógica de carrinho

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
+
+import { LoggingService } from '../logging.service';
+import { Ingredient } from '../shared/ingredient.model';
+import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions';
+import * as fromApp from '../store/app.reducer';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css'],
+})
+export class ShoppingListComponent implements OnInit, OnDestroy {
+  ingredients: Observable<{ ingredients: Ingredient[] }>;
+  private subscription: Subscription;
+
+  constructor(
+    private loggingService: LoggingService,
+    private store: Store<fromApp.AppState>
+  ) {}
+
+  ngOnInit(): void {
+    this.ingredients = this.store.select('shoppingList');
+    // this.ingredients = this.slService.getIngredients();
+    // this.subscription = this.slService
+    //   .getIngredientsChanged()
+    //   .subscribe((ingredients: Ingredient[]) => {
+    //     this.ingredients = ingredients;
+    //   });
+    this.loggingService.printLog('Hello from ShoppingListComponent ngOnInit');
+  }
+
+  onEditItem(index: number) {
+    // this.slService.startedEditing.next(index);
+    this.store.dispatch(new ShoppingListActions.StartEdit(index));
+  }
+
+  ngOnDestroy(): void {
+    // this.subscription.unsubscribe();
+  }
+}
